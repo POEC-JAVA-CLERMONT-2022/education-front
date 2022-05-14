6da9e74a506e6d0581fd52127882d98e8dbe69f1
Utilisateur <soubla19@hotmail.fr>
2022-05-14 16:57:34 +0200

    lesson.service

---
+++ src/services/url-api-constantes.ts
@@ -0,0 +1,17 @@
+import {HttpHeaders} from "@angular/common/http";
+
+export class UrlApiConstantes {
+
+  /** Header */
+  static headers: {headers: HttpHeaders} = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/ld+json',
+    })
+  };
+
+  /** SERVICES */
+  static urlApi: string = 'http://localhost:8080';
+  static urlApiLessons: string = '/lessons';
+
+
+  }
