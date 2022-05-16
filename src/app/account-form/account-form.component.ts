f26cb7f0a162e8ba970e3c78872be1770504a008
Utilisateur <soubla19@hotmail.fr>
2022-05-16 08:54:17 +0200

    rien

--- src/app/account-form/account-form.component.ts
+++ src/app/account-form/account-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpLessonService} from "../../services/lesson.service";
+import {HttpUserService} from "../../services/user.service";
+import {Lesson} from "../../models/lesson";
+import {User} from "../../models/user";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-account-form',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-form.component.scss']
 })
 export class AccountFormComponent implements OnInit {
+  paramEmail: string|undefined;
+  users: Array<User> = [];
+  user: User|undefined;
 
-  constructor() { }
+  constructor(private httpUser: HttpUserService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+   /* this.activatedRoute.params.subscribe((params) => {
+      this.paramEmail=params.email;
+      if(this.paramEmail) {
+        this.httpUser.getUserByEmail(this.paramEmail).subscribe((user) => {
+          this.user = user;
+        });
+        }
+      }); */
   }
+
+
 
 }
